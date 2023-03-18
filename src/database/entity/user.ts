9e657ffe2075d6e46fa2c5e81bb56e5339a358b8
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-03-18 11:13:42 +0500

    refactoring the test files for better readability

--- src/database/entity/user.ts
+++ src/database/entity/user.ts
@@ -1,7 +1,24 @@
-import { Entity, PrimaryGeneratedColumn, Column, Unique  } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
 
 @Entity()
 @Unique(['username'])
-export class User{
-    
+export class User {
+
+    constructor() {
+
+    }
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 255 })
+    username: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    password: string;
+
+    @Column({ type: 'float', default: 0 })
+    deposit: number;
+
+    // @Column({ type: 'enum', enum: UserRole })
+    // role: UserRole;
 }
