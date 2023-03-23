a79017b5ea2be4448f211bd62b0aa545d9961aba
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-03-23 10:21:53 +0500

    removing ormconfig and typeorm declarations

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,13 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import { user } from "./entity/User";
+import { Sequelize } from 'sequelize-typescript';
 
-const options: DataSourceOptions = {
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "password_P12345",
-    database: "testDb",
-    synchronize: true,
-    logging: true,
-    entities: [user],
-    subscribers: [],
-    migrations: [],
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'password_P12345',
+  database: 'testdb',
+  models: [__dirname + '/models'], // Update this path to point to your Sequelize models folder
+});
 
-}
-
-export const db = new DataSource(options);
+export default sequelize;
